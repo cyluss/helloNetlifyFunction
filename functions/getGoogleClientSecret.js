dcedc1a0344c940b2d9ddb7f5b0aa220ec9ffb90
cyluss <cyluss@gmail.com>
2022-08-16 21:07:41 +0900

    add cors

--- functions/getGoogleClientSecret.js
+++ functions/getGoogleClientSecret.js
@@ -1,14 +1,28 @@
 
 const handler = async function (event, context) {
-  const { identity, user } = context.clientContext;
-
-  try {
-  console.log(user)
-  const data = { value: process.env.GOOGLE_CLIENT_SECRET }
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ identity, user, msg: data.value }),
+  // CORS
+  if (event.httpMethod === "OPTIONS") {
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type"
+      }
+    };
   }
+
+  const { identity, user } = context.clientContext;
+  try {
+    if (user === null) {
+      return {
+        statusCode: 401
+      };
+    }
+    const data = { value: process.env.GOOGLE_CLIENT_SECRET };
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ identity, user, msg: data.value }),
+    }
   } catch (error) {
     console.log(error)
     return {
